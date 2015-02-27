c37d0fa2da97336ab863b552209ff0fa3f7a3704
Adrien Louis-Rossignol <adrien.louis.r@gmail.com>
2015-02-27 16:04:24 +0100

    Add roles and users seed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150227133702) do
+ActiveRecord::Schema.define(version: 20150227145501) do
 
   create_table "orders", force: :cascade do |t|
     t.decimal  "total_price"
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(version: 20150227133702) do
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,6 +45,7 @@ ActiveRecord::Schema.define(version: 20150227133702) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "role_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
