72a2afd24223e0e04c58314eb284268379e250e2
Adrien Louis-Rossignol <adrien.louis.r@gmail.com>
2015-03-06 18:05:38 +0100

    Add orders service

--- app/services/delivery_service.rb
+++ app/services/delivery_service.rb
@@ -1,23 +1,22 @@
 class DeliveryService
   def get_orders_to_deliver
-    status = Status.find_by_label('is_paid')
-    status.orders unless status.nil?
+    order_model.includes(:status).where(statuses: {label: %w(is_new is_paid) })
   end
 
   def get_address_for_orders(orders_id)
-    external_order_service.find(orders_id).map { |order| order.address}
+    order_model.find(orders_id).map { |order| order.address}
   end
 
   def set_order_delivered(order_id)
-    status_id = external_status_service.find_by_label('is_delivered').id
-    external_order_service.find(order_id).update(status_id: status_id)
+    status_id = status_modal.find_by_label('is_delivered').id
+    order_model.find(order_id).update(status_id: status_id)
   end
 
   private
-  def external_order_service
+  def order_model
     Order
   end
-  def external_status_service
+  def status_modal
     Status
   end
 end
